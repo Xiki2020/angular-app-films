2a3d788893a7d374bfca27245c2c43d85a45eb63
Shenja <shenja.chekan@gmail.com>
2022-01-24 23:41:25 +0300

    нашел баг

--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -1,39 +1,39 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
-import {DetailFilm} from "../../model/film.model";
-import {OmdbService} from "../../service/omdb.service";
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { DetailFilm } from "../../model/film.model";
+import { OmdbService } from "../../service/omdb.service";
 
 @Component({
-  selector: 'app-dialog',
-  templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.scss']
+	selector: 'app-dialog',
+	templateUrl: './dialog.component.html',
+	styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent implements OnInit {
 
-  isLoading!: boolean;
-  detailsFilm!: DetailFilm;
+	isLoading!: boolean;
+	detailsFilm!: DetailFilm;
 
-  constructor(
-              public dialogRef: MatDialogRef<DialogComponent>,
-              @Inject(MAT_DIALOG_DATA)
-              public id: string,
-              private omdbService: OmdbService,
-    ) { }
+	constructor(
+		public dialogRef: MatDialogRef<DialogComponent>,
+		@Inject(MAT_DIALOG_DATA)
+		public id: string,
+		private omdbService: OmdbService,
+	) { }
 
-  ngOnInit(): void {
-    this.getFilmById(this.id);
-  }
+	ngOnInit(): void {
+		this.getFilmById(this.id);
+	}
 
-  public onNoClick(): void {
-    this.dialogRef.close();
-  }
+	public onNoClick(): void {
+		this.dialogRef.close();
+	}
 
-  public getFilmById(id: string): void {
-    this.isLoading = true;
-    this.omdbService.getFilmById(id).subscribe((detailsFilm: DetailFilm) => {
-      this.detailsFilm = detailsFilm;
-    })
-    this.isLoading = false;
-  }
+	public getFilmById(id: string): void {
+		this.isLoading = true;
+		this.omdbService.getFilmById(id).subscribe((detailsFilm: DetailFilm) => {
+			this.detailsFilm = detailsFilm;
+			this.isLoading = false;
+		})
+	}
 
 }
